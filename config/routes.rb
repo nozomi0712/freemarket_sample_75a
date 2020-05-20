377e637e6e64ec29f389e02e4ecd1bbea66f463c
nozomi0712 <s.mo.19930712@gmail.com>
2020-05-20 21:35:10 +0900

    setting deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  root  'items#index'
 
-  devise_for :users
+  root to: 'items#index'
   resources :items 
-
+  devise_for :users
   # あとで確認
   # memberにする
   get 'items/purchase', to: 'items#purchase'
+
 end
