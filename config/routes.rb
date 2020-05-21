72dc1e6987091fbf28a489669c53ecc8ffe00dd5
AkihideKato <o8033998770@gmail.com>
2020-05-21 11:34:53 +0900

    devie add  view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   root to: 'items#index'
   resources :items
   resources :users
-  devise_for :users
   # あとで確認
   # memberにする
   get 'items/purchase', to: 'items#purchase'
