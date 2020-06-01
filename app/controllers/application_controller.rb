acab0c4afcbb8b6bd7767ca13f0c88fd13eeeadc
ryoji3333 <superfree1111@yahoo.co.jp>
2020-06-01 10:31:42 +0900

    header-category-menu4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_category
   protected 
-  
+
+  def set_category
+    @parents = Category.where(ancestry: nil)
+  end
+
   private
   
   def basic_auth
@@ -16,7 +21,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:birthday,:first_name,:first_furigana,:last_name,:last_furigana])
   end
-  
+
   def production?
     Rails.env.production?
   end
