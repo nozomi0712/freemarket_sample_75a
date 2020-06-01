f6fd4774f654f27d4720e4ad20022ee3db3bcce0
hiroto0114-maker <62985949+hiroto0114-maker@users.noreply.github.com>
2020-06-01 14:05:51 +0900

    カテゴリー商品

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category=Category.find(params[:id])
-    @goods=Item.where(category_id:params[:id]).order("created_at DESC").page(params[:page]).per(5)
+    @goods=Item.where(category_id:params[:id]).order("created_at DESC").page(params[:page]).per(1)
   end
 end
