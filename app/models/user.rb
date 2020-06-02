3aab82f96f218b0b9b66cc632a17a1d54e7d1bdf
AkihideKato <o8033998770@gmail.com>
2020-06-02 16:56:32 +0900

    fix error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_one   :user_address
   devise    :validatable, password_length: 7..128
   validates :nickname, :birthday, :first_name, :first_furigana, :last_name, :last_furigana ,presence: true
-  validates :first_furigana, :last_furigana, format: { with: /\A[ぁ-んァ-ヶー－]+\z/,message: "には全角で入力してください。"}
-  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥一-龥]+\z/,message: "には全角漢字で入力してください。"} 
+  validates :first_furigana, :last_furigana, format: { with: /\A[ぁ-んァ-ヶー－]+\z/,message: "には ※全角カタカナ※ で入力してください。"}
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥一-龥]+\z/,message: "には ※全角※ で入力してください。"} 
 end
