b27d523ad5b62306bd1cc9e6f36c5be1a4305b9f
AkihideKato <o8033998770@gmail.com>
2020-06-03 22:55:32 +0900

    fix: error message ,add: flash_message , login after view stay_view

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get '/users/:id/edit', to: 'users#edit'
     patch '/user/:id', to: 'users/registrations#update'
     get 'addresses', to: 'users/registrations#new_address'
-    get '/user_addresses/:id/edit', to: 'users/registrations#edit_user_addresses'
+    get '/user_addresses/edit', to: 'users/registrations#edit_user_addresses'
     patch '/user_addresses/:id', to: 'users/registrations#update_user_addresses'
     post 'addresses', to: 'users/registrations#create_address'
     post '/user_addresses/:id', to: 'users/registrations#update_user_addresses'
