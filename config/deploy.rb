24c19010588c2b5b3981e3dc7796c227fe57c58e
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-04 11:01:37 +0900

    edit deploy.rb capistrano migrate

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,9 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
 set :linked_files, %w{ config/master.key }
+
+set :migration_role, 'db'
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
