3208105e72e7ac64804f4a4bdfbb1ea32d114078
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-08 20:11:40 +0900

    create-rspec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Card do
+  let(:user)  {create(:user)}
+  context "cardが登録できること" do
+    it "全て入力されると登録できること" do
+      expect(create(:card)).to be_valid
+    end
+  end
+  context "cardが登録できるない" do
+    it "userがいないと登録できないこと" do
+      card = build(:card, user: nil)
+      card.valid?
+      expect(card.errors[:user]).to include("を入力してください")
+    end
+  end
 end
